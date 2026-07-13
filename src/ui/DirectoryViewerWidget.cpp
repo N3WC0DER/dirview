ef2e5f1a232b491277d748ba7b0f8061feed087c
Savkin Nikita <50752617+N3WC0DER@users.noreply.github.com>
2026-07-13 13:50:27 +0400

    Version 1.3.0 (#7)
    * feat: calculate folder size in context menu

--- src/ui/DirectoryViewerWidget.cpp
+++ src/ui/DirectoryViewerWidget.cpp
@@ -8,7 +8,9 @@ DirectoryViewerWidget::DirectoryViewerWidget(const Options &options, QWidget *pa
     // file models
     ModelFactory modelFactory(options);
     model = modelFactory.createFileSystemModel(rootPath, this);
-    proxyModel = modelFactory.createFilterProxyModel(model, this);
+    filterProxyModel = modelFactory.createFilterProxyModel(model, this);
+    filterProxyModel->setProxyRootPath(model->rootPath());
+    folderSizeProxyModel = modelFactory.createFolderSizeProxyModel(filterProxyModel, this);
 
     // Set home directory after loading
     connect(model, &QFileSystemModel::directoryLoaded,
@@ -24,7 +26,7 @@ DirectoryViewerWidget::DirectoryViewerWidget(const Options &options, QWidget *pa
     // Filter line
     filter = new QLineEdit(this);
     filter->setPlaceholderText(tr("Фильтр"));
-    auto *filterController = new FilterController(proxyModel, filter, this);
+    auto *filterController = new FilterController(filterProxyModel, filter, this);
     layout->addWidget(filter);
 
     configureTreeView();
@@ -37,18 +39,65 @@ DirectoryViewerWidget::DirectoryViewerWidget(const Options &options, QWidget *pa
 
 void DirectoryViewerWidget::configureTreeView() {
     tree = new QTreeView(this);
-    tree->setModel(proxyModel);
+    tree->setModel(folderSizeProxyModel);
 
     tree->setSortingEnabled(true);
     tree->header()->setSectionResizeMode(0, QHeaderView::Stretch);
     tree->header()->setSectionResizeMode(1, QHeaderView::Interactive);
+
+    tree->setContextMenuPolicy(Qt::CustomContextMenu);
+
+    connect(tree, &QTreeView::customContextMenuRequested,
+        this, &DirectoryViewerWidget::onDirContextMenuRequested);
+}
+
+void DirectoryViewerWidget::calculateFolderSize(const QString &folderPath) {
+    QFuture<quint64> future = QtConcurrent::run(getDirSize, folderPath);
+            
+    auto *watcher = new QFutureWatcher<quint64>(this);
+    connect(watcher, &QFutureWatcher<quint64>::finished, this, [this, watcher, folderPath]() {
+        quint64 totalSize = watcher->result();
+        
+        folderSizeProxyModel->updateFolderSize(folderPath, totalSize);
+        
+        watcher->deleteLater();
+    });
+    watcher->setFuture(future);
 }
 
 void DirectoryViewerWidget::setHomeDirectory(const QString &loadedPath) {
     if (loadedPath == rootPath) {
         QModelIndex rootIndex = model->index(loadedPath);
         if (rootIndex.isValid()) {
-            tree->setRootIndex(proxyModel->mapFromSource(rootIndex));
+            QModelIndex filterProxyIndex = filterProxyModel->mapFromSource(rootIndex);
+            QModelIndex folderSizeProxyIndex = folderSizeProxyModel->mapFromSource(filterProxyIndex);
+            tree->setRootIndex(folderSizeProxyIndex);
+        }
+    }
+}
+
+void DirectoryViewerWidget::onDirContextMenuRequested(const QPoint &point) {
+    QModelIndex folderSizeProxyIndex = tree->indexAt(point);
+    if (!folderSizeProxyIndex.isValid()) return;
+
+    QModelIndex filterProxyIndex = folderSizeProxyModel->mapToSource(folderSizeProxyIndex);
+    if (!filterProxyIndex.isValid()) return;
+
+    QModelIndex index = filterProxyModel->mapToSource(filterProxyIndex);
+    if(!index.isValid()) return;
+
+    QString absolutePath = model->filePath(index);
+    QFileInfo fileInfo(absolutePath);
+
+    // Контекстное меню для директории
+    if (fileInfo.isDir()) {
+        QMenu contextMenu(this);
+        QAction *calculateDirSize = contextMenu.addAction(tr("Вычислить размер папки"));
+
+        QAction *selectedAction = contextMenu.exec(tree->viewport()->mapToGlobal(point));
+
+        if (selectedAction == calculateDirSize) {
+            calculateFolderSize(absolutePath);
         }
     }
 }
---
+++ src/utils/CalculateSize.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <QDirIterator>
+
+/**
+ * @brief Получает общий размер файлов в указанной директории.
+ * Рекомендуется выполнять асинхронно.
+ * 
+ * @param dirPath Путь до папки.
+ * @return quint64 Размер папки.
+ */
+inline quint64 getDirSize(const QString &dirPath) {
+    quint64 size = 0;
+    QDirIterator it(dirPath, QDir::Files | QDir::NoDotAndDotDot, QDirIterator::Subdirectories);
+    while (it.hasNext()) {
+        it.next();
+        size += it.fileInfo().size();
+    }
+    return size;
+}
